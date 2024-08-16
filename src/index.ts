3d1523093ebd3b3ebbdddbea21f21a333f69dd57
kilkuwu <iamkilkuwu@gmail.com>
2024-08-16 16:34:21 +0700

    ok

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import { Hono, Context } from 'hono';
 import verifyDiscordRequest from './middlewares/verifyDiscordRequest';
 
-import { commandMap } from './commands';
 import apiFromToken from './api';
 import {
 	APIChatInputApplicationCommandInteraction,
 	APIInteraction,
+	ApplicationCommandType,
 	InteractionResponseType,
 	InteractionType,
 } from 'discord-api-types/v10';
 import E from './types/Env';
+import chatInputApplicationCommandHandler from './handlers/chatInputApplicationCommandHandler';
+import modalSubmitHandler from './handlers/modalSubmitHandler';
 
 const app = new Hono<E>();
 
@@ -22,60 +24,49 @@ app.get('/', async (c) => {
 	return c.text(`Hello ${c.env.DISCORD_APPLICATION_ID}`);
 });
 
+app.get('/test', async (c) => {
+	const filters = (await c.env.KV_STORE.get(
+		'filters',
+		'json'
+	)) as any;
+	console.log(filters);
+	return c.json(filters);
+});
+
+app.get('/test2', async (c) => {
+	await c.env.KV_STORE.put(
+		'filters',
+		JSON.stringify({
+			basic:
+				"-gay* -futa* -anthro*   -interspecies*  -animal* -feline* -comic* -monster* -winx* -size_difference -pokemon_(species) -alicorn* -king_of_the_hill  -cartoon* -male_only  -powerpuff_girls -wind_waker  -nickelodeon -rissma_(maewix1) -hazbin_hotel -furry -idw* -five_nights_at_freddy's -giratina -pseudoregalia -mass_effect -gacha  -tentacle* -ed_edd_n_eddy score:>=100",
+		})
+	);
+	return c.text('Done!');
+});
+
 app.post('/', verifyDiscordRequest, async (c) => {
 	const interaction: APIInteraction = await c.req.json();
-
 	if (interaction.type == InteractionType.Ping) {
 		return c.json({
 			type: InteractionResponseType.Pong,
 		});
 	}
-
 	if (
 		interaction.type === InteractionType.ApplicationCommand
 	) {
-		const name = interaction.data.name.toLowerCase();
-		const command = commandMap.get(name);
-
-		if (!command) {
-			return c.json({ error: 'Unknown Command' }, 400);
-		}
-
-		const options =
-			(
+		if (
+			interaction.data.type ==
+			ApplicationCommandType.ChatInput
+		) {
+			return await chatInputApplicationCommandHandler(
+				c,
 				interaction as APIChatInputApplicationCommandInteraction
-			).data.options || [];
-
-		const inputMap = new Map<string, any>();
-
-		for (const opt of options) {
-			const option = opt as {
-				name: string;
-				type: number;
-				value: any;
-			};
-			if (option.value) {
-				inputMap.set(option.name, option.value);
-			}
+			);
 		}
-
-		c.executionCtx.waitUntil(
-			(async () => {
-				while (c.res.status != 200) {
-					await new Promise<void>((f) => f());
-				} // wait for the defer to be finshed
-
-				await command.run(
-					c,
-					interaction as APIChatInputApplicationCommandInteraction,
-					inputMap
-				);
-			})()
-		);
-
-		return c.json({
-			type: InteractionResponseType.DeferredChannelMessageWithSource,
-		});
+	} else if (
+		interaction.type == InteractionType.ModalSubmit
+	) {
+		return await modalSubmitHandler(c, interaction);
 	}
 
 	return c.json({ error: 'Unknown Type' }, 400);
